0ab7db75fa601423ca5d7774d0435497c3acc631
Andy Meneely <andy.meneely@gmail.com>
2022-11-29 18:17:55 -0500

    update game icons gem

--- src/hello.rb
+++ src/hello.rb
@@ -1,7 +1,8 @@
 require 'squib'
 
-Squib::Deck.new do
+Squib::Deck.new(cards: 2) do
 	background color: :white
-	text str: 'Hello, world!'
+
+	text str: ['Hello', 'world!']
 	save_png prefix: 'hello_'
 end
